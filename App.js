34e590ed4f8f84185d9bcb590e5f9712f4a7bba9
aman mirza <aman mirza99>
2020-05-12 14:12:35 +0500

    inline style part-1

--- App.js
+++ App.js
@@ -24,59 +24,93 @@
 // }
 
 // ========================Section 2 flexbox detail====================
+// import React from "react";
+// import { Text, View } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View
+//       style={{
+//         padding: 50,
+//         flexDirection: "row",
+//         width: "80%",
+//         height: 300,
+//         justifyContent: "space-around",
+//         alignItems: "stretch",
+//       }}
+//     >
+//       <View
+//         style={{
+//           backgroundColor: "red",
+//           // width: 100,
+//           // height: 100,
+//           flex:1,
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>1</Text>
+//       </View>
+//       <View
+//         style={{
+//           backgroundColor: "blue",
+//           // width: 100,
+//           // height: 100,
+//           flex:1,
+
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>2</Text>
+//       </View>
+//       <View
+//         style={{
+//           backgroundColor: "green",
+//           // width: 100,
+//           // height: 100,
+//           flex:1,
+
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>3</Text>
+//       </View>
+//     </View>
+//   );
+// =========================Section 3=======================
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: "row",
-        width: "80%",
-        height: 300,
-        justifyContent: "space-around",
-        alignItems: "stretch",
-      }}
-    >
+    <View style={styles.screen}>
       <View
-        style={{
-          backgroundColor: "red",
-          // width: 100,
-          // height: 100,
-          flex:1, 
-          justifyContent: "center",
-          alignItems: "center",
-        }} 
+        style={styles.inputContainer}
       >
-        <Text>1</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          // width: 100,
-          // height: 100,
-          flex:1, 
-
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "green",
-          // width: 100,
-          // height: 100,
-          flex:1, 
-
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
+        <TextInput
+          placeholder="course Goal"
+          style={styles.TextInput}
+        />
+        <Button title="ADD" />
       </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  screen:{
+    padding:50
+  },
+  inputContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  TextInput:{
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  }
+});
