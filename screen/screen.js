0a93b7609aebf4466c54ec8144bcbca9fab1b617
aman mirza <aman mirza99>
2020-05-15 01:37:37 +0500

    Number guessing app (header,screen,layout)  part-11

--- screen/screen.js
+++ screen/screen.js
@@ -5,8 +5,14 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 const GameScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>Start a New Game!</Text>
-      
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View  style={styles.input}>
+        <TextInput/>
+        <View style={styles.button}>
+            <Button title="Reset" onPress={()=>{}}/>
+            <Button title="Confirm" onPress={()=>{}}/>
+        </View>
+      </View>
     </View>
   );
 };
@@ -16,6 +22,23 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center'
+  },
+  title:{
+      fontSize:20,
+      marginVertical:10
+  },
+  button:{
+     flexDirection:'row',
+     width:"100%",
+     justifyContent:"space-between",
+     paddingHorizontal:15,
+  },
+  input:{
+    width:300,
+    maxWidth:"80%",
+    alignItems:"center",
+    borderColor:"black"
+
   }
 });
 
