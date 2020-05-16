ab57fcae6ab0101297b87c4ed755d72bf451cd96
aman mirza <aman mirza99>
2020-05-17 00:45:59 +0500

    switiching between multiple screens  part-18

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './components/header';
 import { StyleSheet, Text, View } from 'react-native';
 import GameScreen from './screen/screen';
-
+import Game from './screen/Game';
 export default function App() {
+  const [userNumber,setUserNumber]=useState();
+
+  const StartGameHandler=(selectedNumber)=>{
+    setUserNumber(selectedNumber)
+  }
+
+  let content =<Game onStartGame={StartGameHandler}/>
+  if(userNumber){
+    content=<GameScreen userChoice={userNumber}/>
+  }
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
-      <GameScreen/>
+      {content}
     </View>
   );
 }
